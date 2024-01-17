a00dea108179b0fbd4ef23cb4b230d952bad7942
M0H1K4N <nejer911@gmail.com>
2024-01-17 19:28:36 +0400

    adding react router

--- front-end/src/components/signUp.js
+++ front-end/src/components/signUp.js
@@ -1,50 +1,50 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { useState } from "react";
 import FreePage from "./freePage";
 
-
 export default function SignUp() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
 
-  const username = "mohikan"
-  const password = "luka123"
+  const [signUp, setSignUp] = useState("");
 
-  const [signUp, setSignUp] = useState()
-
-  const newDiv = () =>{
-    <div>
-      it is workig bro
-    </div>
-  }
-
-  if(username === "mohikan" && password === "luka123"){
-
-  }
+  const newDiv = () => {
+    if (username === "mohikan" && password === "luka123") {
+      history.push("/newPage");
+    } else {
+      console.log("there is something wrong");
+    }
+  };
 
   return (
-    <div className="app-container"> 
+    <div className="app-container">
       <h1>Chat App</h1>
       <form className="app-form">
         <div className="form-group">
           <label htmlFor="username">Username:</label>
           <input
-            username={username}
-            password={password}
+            value={username}
             type="text"
             id="username"
             name="username"
             placeholder="Enter your username"
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password:</label>
           <input
+            value={password}
             type="password"
             id="password"
             name="password"
             placeholder="Enter your password"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button type="submit" className="app-btn">
+        <button type="submit" className="app-btn" onClick={newDiv}>
           Login
         </button>
       </form>
