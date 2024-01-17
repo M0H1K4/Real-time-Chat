a7903b12ddd23e762f0603bb8a5baf08fec28187
M0H1K4N <nejer911@gmail.com>
2024-01-17 19:49:20 +0400

    adding react router

---
+++ front-end/src/components/newPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function newPage() {
+  return (
+    <div>
+      <h1>This is new page and succesfully working</h1>
+    </div>
+  );
+}
--- front-end/src/components/signUp.js
+++ front-end/src/components/signUp.js
@@ -1,53 +1,64 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 import { useState } from "react";
-import FreePage from "./freePage";
+import NewPage from "./newPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function SignUp() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const history = useHistory();
-
-  const [signUp, setSignUp] = useState("");
 
   const newDiv = () => {
     if (username === "mohikan" && password === "luka123") {
-      history.push("/newPage");
+      // Redirect to the new page
+      window.location.href = "/newPage";
     } else {
       console.log("there is something wrong");
     }
   };
 
   return (
-    <div className="app-container">
-      <h1>Chat App</h1>
-      <form className="app-form">
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            value={username}
-            type="text"
-            id="username"
-            name="username"
-            placeholder="Enter your username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            value={password}
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Enter your password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="app-btn" onClick={newDiv}>
-          Login
-        </button>
-      </form>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="app-container">
+              <h1>Chat App</h1>
+              <form className="app-form">
+                <div className="form-group">
+                  <label htmlFor="username">Username:</label>
+                  <input
+                    value={username}
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="Enter your username"
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password:</label>
+                  <input
+                    value={password}
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <button type="button" className="app-btn" onClick={newDiv}>
+                  Login
+                </button>
+              </form>
+            </div>
+          }
+        />
+        <Route>
+          path= "/newPage"
+          element = { <NewPage/> }
+        </Route>
+      </Routes>
+    </Router>
   );
 }
