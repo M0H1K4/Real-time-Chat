8f502c928da4835bd3f9a04df92a5b7ed480506d
M0H1K4N <nejer911@gmail.com>
2024-01-18 20:48:30 +0400

    Add

--- front-end/src/components/signUp.js
+++ front-end/src/components/signUp.js
@@ -53,7 +53,7 @@ export default function SignUp() {
                     href="#https://www.youtube.com/watch?v=vIklNRKQ1l0"
                     target="_blank"
                   >
-                    Forgot Password?
+                    Forgot Password?  no
                   </a>
                 </span>
                 <button type="button" className="app-btn" onClick={newDiv}>
