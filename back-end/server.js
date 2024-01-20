216b5a968bf968b4611b22905c6fe219fef74e69
M0H1K4N <nejer911@gmail.com>
2024-01-20 22:05:25 +0400

    Add

--- back-end/server.js
+++ back-end/server.js
@@ -3,9 +3,22 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
-
 const URL = 'mongodb+srv://Mohikan:luka123@cluster1.juf2fhu.mongodb.net/?retryWrites=true&w=majority'
- 
+const { Schema } = mongoose;
+
+
+const blogSchema = new Schema({
+  title: String, // String is shorthand for {type: String}
+  author: String,
+  body: String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs: Number
+  }
+});
 
 async function connect() {
   try{
