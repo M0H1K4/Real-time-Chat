1a3875148139e29fb4c628a5ff2415303c0b8bac
M0H1K4N <nejer911@gmail.com>
2024-01-21 17:26:54 +0400

    added body parser

--- back-end/server.js
+++ back-end/server.js
@@ -3,38 +3,55 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
-const URL = 'mongodb+srv://Mohikan:luka123@cluster1.juf2fhu.mongodb.net/?retryWrites=true&w=majority'
+const URL =
+"mongodb+srv://Mohikan:luka123@cluster1.juf2fhu.mongodb.net/?retryWrites=true&w=majority";
 const { Schema } = mongoose;
+const bodyParser = require("body-parser");
 
-
-const blogSchema = new Schema({
-  name: String, // String is shorthand for {type: String}
-  secondName: String,
-  body: String,
-});
+// Parse incoming JSON requests
+app.use(bodyParser.json());
 
 async function connect() {
-  try{
-    await mongoose.connect(URL)
-    console.log('connected to DB')
-  } catch(err){
-    console.log(err)
+  try {
+    await mongoose.connect(URL);
+    console.log("connected to DB");
+  } catch (err) {
+    console.log(err);
   }
-}
+} 
+
+
+const users = [];
+const userSchema = new Schema({
+  name: String, // String is shorthand for {type: String}
+  email: String,
+  body: String,
+});
+const User = model("User", userSchema)
 
 app
-  .get("/", (req, res) => {
-    res.end("Hello, world!");
+  .get("/users", async(req, res) => {
+    try{
+      const users = await User.find();
+      res.json(users);
+    }catch(error){
+      res.status(500).json({error: "Internal Server ERROR"})
+    }
   })
-  .listen(PORT);
-
-
-app.post("/", (req, res) => {
   
-})
+  
 
+app.post("/users/newUser", (req, res) => {
+  const [name, email] = req.body
 
+  if(!name || !email) {
+    return res.status(400).json({error:"Name and email are required"})
+  }
 
+  const newUser = {id: users.length + 1 , name, email }
+  users.push(newUser)
 
-  connect();
+  return res.status(201).json(newUser)
+});
 
+connect();
