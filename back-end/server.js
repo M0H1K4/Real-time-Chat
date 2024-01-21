b6cc1452d2906d6bb6437c958afe2fdd6a35cbf7
M0H1K4N <nejer911@gmail.com>
2024-01-21 17:36:17 +0400

    post method added

--- back-end/server.js
+++ back-end/server.js
@@ -1,15 +1,22 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 const app = express();
 const PORT = 3000;
-const URL =
-"mongodb+srv://Mohikan:luka123@cluster1.juf2fhu.mongodb.net/?retryWrites=true&w=majority";
-const { Schema } = mongoose;
+const URL = "mongodb+srv://Mohikan:luka123@cluster1.juf2fhu.mongodb.net/?retryWrites=true&w=majority";
 const bodyParser = require("body-parser");
 
 // Parse incoming JSON requests
 app.use(bodyParser.json());
+
+// MongoDB Schema Declaration
+const userSchema = new Schema({
+  name: String,
+  email: String,
+});
+
+// MongoDB Model based on the schema
+const User = model("User", userSchema);
 
 async function connect() {
   try {
@@ -18,40 +25,34 @@ async function connect() {
   } catch (err) {
     console.log(err);
   }
-} 
+}
 
-
-const users = [];
-const userSchema = new Schema({
-  name: String, // String is shorthand for {type: String}
-  email: String,
-  body: String,
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
-const User = model("User", userSchema)
 
-app
-  .get("/users", async(req, res) => {
-    try{
-      const users = await User.find();
-      res.json(users);
-    }catch(error){
-      res.status(500).json({error: "Internal Server ERROR"})
-    }
-  })
-  
-  
+app.post("/users/newUser", async (req, res) => {
+  const { name, email } = req.body;
 
-app.post("/users/newUser", (req, res) => {
-  const [name, email] = req.body
-
-  if(!name || !email) {
-    return res.status(400).json({error:"Name and email are required"})
+  if (!name || !email) {
+    return res.status(400).json({ error: "Name and email are required" });
   }
 
-  const newUser = {id: users.length + 1 , name, email }
-  users.push(newUser)
+  try {
+    const newUser = await User.create({ name, email });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
-  return res.status(201).json(newUser)
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 connect();
