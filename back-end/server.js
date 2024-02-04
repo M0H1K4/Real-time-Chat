62011b2bde32f1b563256d7f39f6e000e16f1509
M0H1K4N <nejer911@gmail.com>
2024-02-04 23:33:52 +0400

    Add

--- back-end/server.js
+++ back-end/server.js
@@ -6,9 +6,11 @@ const PORT = 3000;
 const URL = "mongodb+srv://Mohikan:luka123@cluster1.juf2fhu.mongodb.net/?retryWrites=true&w=majority";
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
+const cors = require("cors");
 
 // Parse incoming JSON requests
 app.use(bodyParser.json());
+app.use(cors());
 
 // MongoDB Schema Declaration
 const userSchema = new Schema({
@@ -23,7 +25,7 @@ const User = model("User", userSchema);
 async function connect() {
   try {
     await mongoose.connect(URL);
-    console.log("connected to DB");
+    console.log("Connected to DB");
   } catch (err) {
     console.log(err);
   }
@@ -38,18 +40,44 @@ app.get("/users", async (req, res) => {
   }
 });
 
-app.post("/users/newUser", async (req, res) => {
-  const { name, email, password } = req.body;
 
-  if (!name || !email || !password) {
-    return res.status(400).json({ error: "Name and email are required" });
+app.post("/users/newUser", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ error: "Username, email, and password are required" });
   }
 
   try {
-    const hashedPassword = await bcrypt.hash(password, 10)
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create({ name, email, password:hashedPassword });
+    const newUser = await User.create({ username, email, password: hashedPassword });
     res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Find the user by username
+    const existingUser = await User.findOne({ username });
+
+    if (!existingUser) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    // Compare the provided password with the hashed password in the database
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    // Password is valid, respond with success
+    res.status(200).json({ message: "Login successful" });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
